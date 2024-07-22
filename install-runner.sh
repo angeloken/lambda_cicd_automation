41f59aff3f4c58979a40789f737ca4064afedf58
angeloken <angeloken17@gmail.com>
2024-07-22 20:03:39 +0700

    initial commit

--- install-runner.sh
+++ install-runner.sh
@@ -2,7 +2,7 @@
 
 ./config.sh --url https://github.com/angeloken/lambda_cicd_automation --token AN27UQO4CSQ77OCZ4TXYDN3GTZR42 --unattended
 
-sudo ./svc.sh install
-sudo ./svc.sh start
+./svc.sh install
+./svc.sh start
 
 tail -f /dev/null
