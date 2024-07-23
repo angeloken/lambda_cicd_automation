03215175bcc302ccb5ea1ad57914600de278f823
angeloken <angeloken17@gmail.com>
2024-07-23 13:49:33 +0700

    initial commit

--- install-runner.sh
+++ install-runner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export RUNNER_ALLOW_RUNASROOT="1"
-./config.sh --url https://github.com/angeloken/lambda_cicd_automation --token AN27UQPWRWV4G6YIC64565LGT5EQ2 --unattended
+./config.sh --url https://github.com/angeloken/lambda_cicd_automation --token AN27UQIENEXIUGHZRYQLB4DGT5NJM --unattended
 ./run.sh
